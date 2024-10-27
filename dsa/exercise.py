a6082b520cac6241c5534d5a0320ad20c225c0fc
mykkelol <1micheal.sihavong2@gmail.com>
2024-10-27 14:45:16 -0700

    more

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -1,110 +1,42 @@
 # ************* LinkedList *************
 # Created LinkedList and Node classes
 # LinkedList class should have append, prepend, pop_first, pop, get, set_value, insert, remove, reverse, and print_list methods
+""" Also do
+    find_middle_node()
+    has_loop()
+    find_kth_from_end()
+    partition_list()
+    remove_duplicates()
+    binary_to_decimal()
+    reverse_between()
+"""
 
+# ************* Doubly Linked List *************
+# Created DoublyLinkedList and Node classes
+# DLL class should have append, pop, prepend, pop_first, get, set_value, insert, remove, and print_list methods
+""" Also do
+    swap_first_last()
+    reverse()
+    is_palindrome()
+    pairwise_swap_values()
+    pairwise_swap_nodes()
+"""
 
-# ************* LinkedList *************
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
+# ************* Stacks & Queues *************
+# Created Stack, Queues, and Node classes
+# Stack class should have push, pop, and print_stack
+# Queue class should have enqueue, dequeue, and print_queue
+""" Also do for stack
+    is_balanced_parentheses()
+    reverse_string()
+    sort_stack()
+    size()
+    is_empty()
+"""
+""" Also do for queue usiing stack (e.g. stack1, stack2)
+    enqueue()
+    dequeue()
+    peek()
+    is_empty()
+"""
 
-class LinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length += 1
-        return True
-    
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head = new_node
-        self.length += 1
-        return True
-    
-    def pop_first(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        self.head = self.head.next
-        temp.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.tail = None
-        return temp
-    
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        pre = self.head
-        while temp.next:
-            pre = temp
-            temp = temp.next
-        self.tail = pre
-        self.tail.next = None
-        self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
-        return pre
-
-    def get(self, index):
-        if index < 0 or index >= self.length:
-            return None
-        temp = self.head
-        for _ in range(index):
-            temp = temp.next
-        return temp
-    
-    def set_value(self, index, value):
-        temp = self.get(index)
-        if temp is None:
-            return False
-        temp.value = value
-        return True
-    
-    def insert(self, index, value):
-        if index < 0 or index >= self.length:
-            return False
-        if index == 0:
-            return self.prepend(value)
-        if index == 1:
-            return self.append(value)
-        new_node = Node(value)
-        temp = self.get(index - 1)
-        new_node.next = temp.next
-        temp.next = new_node
-        self.length += 1
-        return True
-    
-    def remove(self, index):
-        if index < 0 or index >= self.length:
-            return False
-        if index == 0:
-            return self.pop_first()
-        if index == 1:
-            return self.pop()
-        pre = self.get(index - 1)
-        temp = pre.next
-        pre.next = temp.next
-        temp.next = None
-        self.length -= 1
-        return True
-    
