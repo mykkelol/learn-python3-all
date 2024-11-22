0682b07331b66700a6ff3141fa5041f28881dda3
mykkelol <1micheal.sihavong2@gmail.com>
2024-11-21 17:57:54 -0800

    add dll prepend

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -153,6 +153,18 @@ class DoublyLinkedList:
             self.tail = new_node
         self.length += 1
         return True
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
 
 # ************* Stacks & Queues *************
 # Create Stack, Queues, and Node classes
