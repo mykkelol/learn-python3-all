936eb6c92c290fb3f945fcd23cf5039bf9ea03c5
mykkelol <1micheal.sihavong2@gmail.com>
2024-12-19 23:09:53 -0800

    remove dupes in ll

--- dsa/run.py
+++ dsa/run.py
@@ -1,16 +1,3 @@
-# ************* LinkedList *************
-# Create LinkedList and Node classes
-# LinkedList class should have append, prepend, pop_first, pop, get, set_value, insert, remove, reverse, and print_list methods
-""" Also do
-    find_middle_node()
-    has_loop()
-    find_kth_from_end()
-    partition_list()
-    remove_duplicates()
-    binary_to_decimal()
-    reverse_between()
-"""
-
 class Node:
     def __init__(self, value):
         self.value = value
@@ -216,11 +203,17 @@ class LinkedList:
         current = self.head
         i = 0
         while current:
-            if i >= start or i <= end:
-                nodes[i] = current
+            if i >= start and i <= end:
+                print(i)
             i += 1
             current = current.next
-    
+        
+        print('--')
+        print(start)
+        print(end)
+        for j, node in enumerate(nodes):
+            print(f'{j} - {nodes.get(j)}')
+
     def print_linked_list(self):
         temp = self.head
         while temp is not None:
@@ -235,196 +228,8 @@ my_ll.append(4)
 my_ll.append(4)
 my_ll.append(5)
 my_ll.print_linked_list()
+print('-----------------')
 my_ll.remove_duplicates()
 my_ll.print_linked_list()
-
-# ************* Doubly Linked List *************
-# Create DoublyLinkedList and Node classes
-# DLL class should have append, pop, prepend, pop_first, get, set_value, insert, remove, and print_list methods
-""" Also do
-    swap_first_last()
-    reverse()
-    is_palindrome()
-    pairwise_swap_values()
-    pairwise_swap_nodes()
-"""
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-        self.prev = None
-
-class DoublyLinkedList:
-    def __init__(self, value):
-        new_node = Node(value)
-        self.head = new_node
-        self.tail = new_node
-        self.length = 1
-
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.prev = self.tail
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length += 1
-        return True
-    
-    def prepend(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            new_node.next = self.head
-            self.head.prev = new_node
-            self.head = new_node
-        self.length += 1
-        return True
-    
-    def pop(self):
-        if self.length == 0:
-            return None
-        temp = self.tail
-        if self.length == 1:
-            self.tail = None
-            self.head = None
-        else:
-            self.tail = self.tail.prev
-            self.tail.next = None
-            temp.prev = None
-        self.length -= 1
-        return temp
-
-    def pop_first(self):
-        if self.length == 0:
-            return None
-        temp = self.head
-        if self.length == 1:
-            self.tail = None
-            self.head = None
-        else:
-            self.head = self.head.next
-            self.head.prev = None
-            temp.next = None
-        self.length -= 1
-        return temp
-    
-    def get(self, index):
-        if index < 0 or index >= self.length:
-            return None
-        temp = self.head
-        if index < self.length/2:
-            for _ in range(index):
-                temp = temp.next
-        else:
-            temp = self.tail
-            for _ in range(self.length - 1, index, -1):
-                temp = temp.prev
-        return temp
-    
-    def set_value(self, index, value):
-        temp = self.get(index)
-        if temp is None:
-            return False
-        temp.value = value
-        return True
-    
-    def insert(self, index, value):
-        if index < 0 or index >= self.length:
-            return False
-        if index == 0:
-            return self.prepend(value)
-        if index == self.length - 1:
-            return self.append(value)
-        
-        new_node = Node(value)
-        temp = self.get(index)
-
-        new_node.next = temp
-        new_node.prev = temp.prev
-        temp.next = None
-        
-        self.length += 1
-        return new_node
-    
-    def remove(self, index):
-        if index < 0 or index >= self.length:
-            return False
-        if index == 0:
-            return self.pop_first()
-        if index == self.length - 1:
-            return self.pop()
-        
-        temp = self.get(index)
-        temp.next.prev = temp.prev
-        temp.prev.next = temp.next
-        temp.next = None
-        temp.prev = None
-
-        self.length -= 1
-        return True
-    
-    def print_doubly(self):
-        temp = self.head
-        while temp.next:
-            print(temp.value)
-            temp = temp.next
-
-# ************* Stacks & Queues *************
-# Create Stack, Queues, and Node classes
-# Stack class should have push, pop, and print_stack
-# Queue class should have enqueue, dequeue, and print_queue
-""" Also do for stack
-    is_balanced_parentheses()
-    reverse_string()
-    sort_stack()
-    size()
-    is_empty()
-"""
-""" Also do for queue usiing stack (e.g. stack1, stack2)
-    enqueue()
-    dequeue()
-    peek()
-    is_empty()
-"""
-
-# ************* Trees *************
-# Create BinarySearchTree and Node classes
-# BinarySearchTree class should have insert and contains
-
-# ************* Hash Tables *************
-# Create HashTable class
-# HashTable class should have __hash, set_item, get_item, get_keys, print_table
-""" Also outside HashTable class do
-    item_in_common
-    find_duplicates
-    first_non_repeating_char
-    group_anagrams
-    two_sum
-    subarray_sum
-    remove_duplicates
-    has_unique_chars
-    find_pairs
-    longest_consecutive_sequence
-"""
-
-# ************* Graphs *************
-# Create Graph class
-# Graph class should have add_vertex, add_edge, remove_edge, remove_vertex, print_graph
-""" Also outside HashTable class do
-    item_in_common
-    find_duplicates
-    first_non_repeating_char
-    group_anagrams
-    two_sum
-    subarray_sum
-    remove_duplicates
-    has_unique_chars
-    find_pairs
-    longest_consecutive_sequence
-"""
+print('-----------------')
+my_ll.reverse_between(1,3)
