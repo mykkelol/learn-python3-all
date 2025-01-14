a82a0f50b0cbe5d015440079f7959ec233b0fdf7
mykkelol <1micheal.sihavong2@gmail.com>
2025-01-13 23:07:21 -0800

    singly linked list

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -11,6 +11,73 @@
     reverse_between()
 """
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+    
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        
+        if self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        
+        temp = self.head
+        pre = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        return temp
+
 # ************* Doubly Linked List *************
 # Create DoublyLinkedList and Node classes
 # DLL class should have append, pop, prepend, pop_first, get, set_value, insert, remove, and print_list methods
