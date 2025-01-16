82591c59e468205dd8dedbeeb3d6d5757116061a
mykkelol <1micheal.sihavong2@gmail.com>
2025-01-15 22:38:45 -0800

    linked list remove

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -107,6 +107,21 @@ class LinkedList:
         self.length += 1
         return True
     
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
     def print_list(self):
         temp = self.head
         while temp:
