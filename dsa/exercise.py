b7e627cf2292cff9282278fc4b3aefdf76347b6a
mykkelol <1micheal.sihavong2@gmail.com>
2025-01-22 23:42:42 -0800

    partition ll

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -149,6 +149,23 @@ class LinkedList:
         # 3 > 2 > 1 > 8 > 5 > 10
         dummy1 = Node(0)
         dummy2 = Node(0)
+        prev1 = dummy1
+        prev2 = dummy2
+        current = self.head
+
+        while current:
+            if current.value < x:
+                prev1.next = current
+                prev1 = current
+            else:
+                prev2.next = current
+                prev2 = current
+            current = current.next
+
+        prev1.next = None
+        prev2.next = None
+        prev1.next = dummy2.next
+        self.head = dummy1.next
 
     def reverse_between(self, start, end):
         dummy = Node(0)
