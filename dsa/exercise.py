fe14c79f763190510eba04e7f39d5eb928d038d8
mykkelol <1micheal.sihavong2@gmail.com>
2025-01-26 15:00:29 -0800

    doubly linked list

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -254,6 +254,31 @@ my_ll.print_list()
     pairwise_swap_nodes()
 """
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
 # ************* Stacks & Queues *************
 # Create Stack, Queues, and Node classes
 # Stack class should have push, pop, and print_stack
