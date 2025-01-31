fc188a9080f54761f54d87ce63416951676ef7aa
mykkelol <1micheal.sihavong2@gmail.com>
2025-01-30 23:49:00 -0800

    doubly insert

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -340,6 +340,25 @@ class DoublyLinkedList:
             return False
         temp.value = value
         return True
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length - 1:
+            return self.append(value)
+
+        new_node = Node(value)
+        temp = self.get(index)
+
+        new_node.prev = temp.prev
+        new_node.next = temp
+        temp.prev.next = new_node
+        temp.prev = new_node
+
+        self.length += 1
+        return temp
 
 # ************* Stacks & Queues *************
 # Create Stack, Queues, and Node classes
