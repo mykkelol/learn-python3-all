b5c76725996034afd26c10179c7d2580ebac979b
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-03-01 15:50:43 -0800

    append ll

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -11,6 +11,29 @@
     reverse_between()
 """
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
+
 # ************* Doubly Linked List *************
 # Create DoublyLinkedList and Node classes
 # DLL class should have append, pop, prepend, pop_first, get, set_value, insert, remove, and print_list methods
