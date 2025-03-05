5d93e50015ca1975f5fa8967a669c4f4daaf6bd6
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-03-04 19:38:06 -0800

    pop ll

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -55,6 +55,23 @@ class LinkedList:
         if self.length == 0:
             self.tail = None
         return temp
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = None
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
 # ************* Doubly Linked List *************
 # Create DoublyLinkedList and Node classes
