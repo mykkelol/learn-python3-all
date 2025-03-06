20e2bee934f8ee96127c044a0277e4a123cbe891
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-03-05 18:22:11 -0800

    get ll

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -59,18 +59,32 @@ class LinkedList:
     def pop(self):
         if self.length == 0:
             return None
+        
+        if self.length == 1:
+            temp = self.tail
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return temp
+        
         temp = self.head
         pre = None
         while temp.next:
             pre = temp
             temp = temp.next
+
         self.tail = pre
         self.tail.next = None
-        temp.next = None
         self.length -= 1
-        if self.length == 0:
-            self.head = None
-            self.tail = None
+        
+        return temp
+    
+    def get(self, index):
+        if index < 0 or self.length >= None:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
         return temp
 
 # ************* Doubly Linked List *************
