e35e62e205b8ea283c09a27ff1d237d2659ba1fd
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-04-02 22:40:48 -0700

    doubly get

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -354,8 +354,13 @@ class DoublyLinkedList:
         if index < 0 or index >= self.length:
             return None
         temp = self.head
-        for _ in range(index):
-            temp = temp.next
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _  in range(self.length - 1, index, -1):
+                temp = temp.prev
         return temp
 
 # ************* Stacks & Queues *************
