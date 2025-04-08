cf5f1b3ca066de6e0a5e3b3dd5017ed53365bb35
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-04-07 21:17:37 -0700

    doubly changes

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -403,6 +403,12 @@ class DoublyLinkedList:
 
         self.length -= 1
         return True
+    
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(temp.value)
+            temp = temp.next
 
 # ************* Stacks & Queues *************
 # Create Stack, Queues, and Node classes
