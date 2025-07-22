597689b5ccc90e344f595a61e64d37e8fd73800d
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-07-21 22:43:32 -0700

    dfs

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -650,6 +650,18 @@ class BinarySearchTree:
                 queue.append(current_node.right)
         return results
 
+    def dfs_pre_order(self):
+        results = []
+        def traverse(current_node):
+            results.append(current_node.value)
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+        traverse(self.root)
+        return results
+    
+
 # ************* Hash Tables *************
 # Create HashTable class
 # HashTable class should have __hash, set_item, get_item, get_keys, print_table
