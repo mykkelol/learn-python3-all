d46d358052cbb858d43fdccf7604edb095751cbc
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-07-23 22:26:12 -0700

    dfs post

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -661,6 +661,16 @@ class BinarySearchTree:
         traverse(self.root)
         return results
     
+    def dfs_post_order(self):
+        results = []
+        def traverse(current_node):
+            if current_node.left is not None:
+                traverse(current_node.left)
+            if current_node.right is not None:
+                traverse(current_node.right)
+            results.append(current_node.value)
+        traverse(self.root)
+        return results
 
 # ************* Hash Tables *************
 # Create HashTable class
