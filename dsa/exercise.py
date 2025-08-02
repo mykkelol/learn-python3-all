1fbf6a9dd2cedfe1261b06cc6f7a0e51dd4d60d9
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-08-02 09:51:22 -0700

    hash init

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -739,6 +739,16 @@ class BinarySearchTree:
     longest_consecutive_sequence
 """
 
+class HashTable:
+    def __init__(self, size=7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+
 # ************* Graphs *************
 # Create Graph class
 # Graph class should have add_vertex, add_edge, remove_edge, remove_vertex, print_graph
