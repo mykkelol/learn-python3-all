b5d7b3e3f6c5f9ed276bde2d159ecc13de26d3d0
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-09-02 22:03:16 -0700

    graph add_vertex

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -851,3 +851,13 @@ class HashTable:
     find_pairs
     longest_consecutive_sequence
 """
+
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list:
+            self.adj_list[vertex] = []
+            return True
+        return False
