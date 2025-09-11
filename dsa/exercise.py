8fe1feb31a64206974c866a2f463f4774f9e21e5
Micheal Sihavong <1micheal.sihavong2@gmail.com>
2025-09-10 19:33:26 -0700

    graph remove vertex

--- dsa/exercise.py
+++ dsa/exercise.py
@@ -875,3 +875,11 @@ class Graph:
             self.adj_list[v2].remove(v1)
             return True
         return False
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list:
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
